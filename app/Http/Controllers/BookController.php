9cda4735a69820fd2a5ef7928a5d8701d2413a30
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-10-23 12:03:01 +0700

    pertemuan 9

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BookController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index']);
+    }
+
+    // table pages
+    public function index()
+    {
+        $jumlahBuku = Book::count();
+        $totalPrice = Book::sum('price');
+        $data_book = Book::all();
+        return view('book.index', compact('data_book', 'jumlahBuku', 'totalPrice'));
+    }
+
+    // to create book view
+    public function create()
+    {
+        return view('book.create');
+    }
+
+    // create book
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'creator' => 'required|string|max:30',
+            'price' => 'required|numeric',
+            'publication_date' => 'required|date'
+        ]);
+
+        $book = new Book();
+        $book->title = $request->title;
+        $book->author_id = Auth::user()->id;
+        $book->price = $request->price;
+        $book->publication_date = $request->publication_date;
+        $book->save();
+
+        return redirect('/book')->with('created', 'Data buku berhasil dibuat');
+    }
+
+    // delete book
+    public function destroy($id){
+        $book = Book::find($id);
+        $book->delete();
+
+        return redirect('/book')->with('deleted', 'Data buku berhasil dihapus');
+    }
+    // to edit book view
+    public function edit($id){
+        $book = Book::find($id);
+        return view('buku.edit', compact('book'));
+    }
+    // update book
+    public function update(Request $request, $id){
+        $book = Book::find($id);
+
+        $request->validate([
+            'title' => 'required|string',
+            'creator' => 'required|string|max:30',
+            'price' => 'required|numeric',
+            'publication_date' => 'required|date'
+        ]);
+
+        $book->title = $request->title;
+        $book->creator = $request->creator;
+        $book->price = $request->price;
+        $book->publication_date = $request->publication_date;
+        $book->save();
+
+        return redirect('/book')->with('updated', 'Data buku berhasil diperbarui');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\BookController;
+use App\Models\Book;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('book', BookController::class);
+
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
+
